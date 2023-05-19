e8a2dc105c9ac4f63a80ae80669a5efd368be950
pixaily <danield.georgiev@gmail.com>
2023-05-19 15:49:30 +0300

    13. Teamwork project - Bomberman v0.2 - Add test level

--- 01.IntroductionToProgramming/13.Teamwork/BomberMan/BomberMan/BomberMan.cs
+++ 01.IntroductionToProgramming/13.Teamwork/BomberMan/BomberMan/BomberMan.cs
@@ -19,90 +19,46 @@ class CleanBomberMan
         }
     }
 
-    // Type 1
-    //public const int PLAYFIELD_WIDTH = 34;
-    //public const int PLAYFIELD_HEIGHT = 27;
-    // Type 2
-    public const int PLAYFIELD_WIDTH = 40;
-    public const int PLAYFIELD_HEIGHT = 33;
-    // Type 3
-    //public const int PLAYFIELD_WIDTH = 46;
-    //public const int PLAYFIELD_HEIGHT = 39;
-
+    public const int PLAYFIELD_WIDTH = 34;
+    public const int PLAYFIELD_HEIGHT = 27;
+    public const int PLAYFIELD_WIDTH1 = 40;
+    public const int PLAYFIELD_HEIGHT1 = 39;
     public const int PLAYFIELD_MAX_WIDTH = PLAYFIELD_WIDTH - 1;
     public const int PLAYFIELD_MAX_HEIGHT = PLAYFIELD_HEIGHT - 1;
     public const int BOX_SIZE = 3;
 
-    public static int PLAYFIELD_WIDTH_BORDER = 1;
-    public static int PLAYFIELD_HEIGHT_BORDER = 1;
+    public const int PLAYFIELD_WIDTH_BORDER = 0;
+    public const int PLAYFIELD_HEIGHT_BORDER = 0;
 
     public const int PLAYFIELD_ROWS = PLAYFIELD_HEIGHT / BOX_SIZE;
     public const int PLAYFIELD_COLS = PLAYFIELD_MAX_WIDTH / BOX_SIZE;
 
     static void Main()
     {
+        Random rnd = new Random();
         // Initilize playfield
         Console.BufferWidth = Console.WindowWidth = PLAYFIELD_WIDTH + 100;
         Console.BufferHeight = Console.WindowHeight = PLAYFIELD_HEIGHT;
         int[,] playfield = new int[PLAYFIELD_HEIGHT, PLAYFIELD_MAX_WIDTH];
 
         // Initizlize level
-        int lvlNumber = 1;
-        int lvlType = 2;
+        int levelType = rnd.Next(1, 2);
+        LevelType(levelType);
 
-        Random rnd = new Random();
-        int subLevel = rnd.Next(1, 3);
 
-        string fileName = GetFileName(Convert.ToString(lvlNumber), Convert.ToString(lvlType), subLevel);
-        string input = File.ReadAllText(fileName);
+        string input = File.ReadAllText("leveltest.txt");
 
         int[,] level = BuildLevel(input);
 
         List<List<int>> emptyBoxes;
         emptyBoxes = GetEmptyBoxes(level);
 
-        int[] playerPositionGameGrid = new int[2];
-
-        List<Position> playerPosition = PlayerInitialization(subLevel, ref playerPositionGameGrid);
-        DrawPlayer(playerPosition);
+        int[,] playerPosition = new int[1, 1];
 
         int numberOfBlocks = 50;
 
         DrawLevel(level);
         DrawBreakableBlock(level, emptyBoxes, numberOfBlocks);
-    }
-
-    private static void DrawPlayer(List<Position> playerPosition)
-    {
-        foreach (Position bodyPart in playerPosition)
-        {
-            Console.SetCursorPosition(bodyPart.X, bodyPart.Y);
-            Console.Write(bodyPart.Symbol);
-        }
-    }
-
-    private static List<Position> PlayerInitialization(int subLevel, ref int[] playerPositionGameGrid)
-    {
-        List<Position> playerPosition = new List<Position>();
-        int x = (PLAYFIELD_WIDTH / PLAYFIELD_WIDTH) + (PLAYFIELD_WIDTH_BORDER * BOX_SIZE) - 1;
-        int y = (PLAYFIELD_HEIGHT / PLAYFIELD_HEIGHT) + (PLAYFIELD_HEIGHT_BORDER * BOX_SIZE) - 1;
-
-        if (subLevel == 2)
-        {
-            x += BOX_SIZE;
-        }
-
-        playerPositionGameGrid[0] = x / BOX_SIZE;
-        playerPositionGameGrid[1] = y / BOX_SIZE;
-
-        playerPosition.Add(new Position(x + 1, y, 'O'));
-        playerPosition.Add(new Position(x, y + 1, '/'));
-        playerPosition.Add(new Position(x + 1, y + 1, '|'));
-        playerPosition.Add(new Position(x + 2, y + 1, '\\'));
-        playerPosition.Add(new Position(x, y + 2, '/'));
-        playerPosition.Add(new Position(x + 2, y + 2, '\\'));
-
-        return playerPosition;
     }
 
     private static void DrawBreakableBlock(int[,] level, List<List<int>> emptyBoxes, int iterations)
@@ -115,12 +71,7 @@ class CleanBomberMan
             int rndRow = rnd.Next(emptyBoxes.Count);
             int rndColIndex = rnd.Next(emptyBoxes[rndRow].Count);
             int matrixRow = rndRow + PLAYFIELD_HEIGHT_BORDER;
-            int matrixCol = emptyBoxes[rndRow][rndColIndex];
-
-            if (PLAYFIELD_WIDTH_BORDER > 0)
-            {
-                matrixCol -= 1;
-            }
+            int matrixCol = emptyBoxes[rndRow][rndColIndex] - PLAYFIELD_WIDTH_BORDER;
 
             int cursorPositionTop = matrixRow * BOX_SIZE;
             for (int i = BOX_SIZE - 1; i >= 0; i--)
@@ -139,7 +90,53 @@ class CleanBomberMan
             iterations--;
         }
     }
-    
+
+    // Works without border
+    //private static void DrawBreakableBlock(int[,] level, List<List<int>> emptyBoxes, int iterations)
+    //{
+    //    Random rnd = new Random();
+
+    //    while (iterations > 0)
+    //    {
+    //        int rndRow = rnd.Next(1, PLAYFIELD_ROWS);
+    //        int rndCol = rnd.Next(1, PLAYFIELD_COLS);
+    //        //int rndRow = 11;
+    //        //int rndCol = 13;
+
+    //        if (emptyBoxes.Count >= rndRow)
+    //        {
+    //            if (emptyBoxes[rndRow - 1].Contains(rndCol - 1))
+    //            {
+    //                emptyBoxes[rndRow - 1].Remove(rndCol - 1);
+
+    //                int cursorPositionTop = (rndRow - 1) * BOX_SIZE;
+
+    //                for (int i = BOX_SIZE - 1; i >= 0; i--)
+    //                {
+    //                    int cursorPositionLeft = (rndCol - 1) * BOX_SIZE;
+    //                    for (int j = BOX_SIZE - 1; j >= 0; j--)
+    //                    {
+    //                        level[cursorPositionTop, cursorPositionLeft] = '2';
+    //                        Console.SetCursorPosition(cursorPositionLeft, cursorPositionTop);
+    //                        Console.Write('2');
+    //                        cursorPositionLeft++;
+    //                    }
+    //                    cursorPositionTop++;
+    //                }
+    //                iterations--;
+    //            }
+    //            else
+    //            {
+    //                rndCol = rnd.Next(0, PLAYFIELD_COLS);
+    //            }
+    //        }
+    //        else
+    //        {
+    //            rndRow = rnd.Next(0, PLAYFIELD_ROWS);
+    //        }
+    //    }
+    //}
+
     private static List<List<int>> GetEmptyBoxes(int[,] level)
     {
         IEnumerable<int> bufferList = new List<int>();
@@ -241,20 +238,16 @@ class CleanBomberMan
 
         return level;
     }
-    
-    private static string GetFileName(string nextLvl, string lvlType, int subLevel)
-    {
-        string fileName = "level" + nextLvl + "-type" + lvlType;
 
-        if (subLevel == 1)
+    private static void LevelType(int n)
+    {
+        if (n == 1)
         {
-            fileName += "-1.txt";
+            Console.WriteLine("Level type with first el is blocked");
         }
-        else
+        else if (n == 2)
         {
-            fileName += "-2.txt";
+            Console.WriteLine("Level type with first el free");
         }
-        
-        return fileName;
     }
 }
