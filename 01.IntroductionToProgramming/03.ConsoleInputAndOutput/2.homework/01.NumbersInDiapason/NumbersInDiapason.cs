cb620d6db3f4231cc7bade38fef2bfdcc8f5f98a
pixaily <danield.georgiev@gmail.com>
2023-05-19 14:36:41 +0300

    3. Console Input and Output - Add homework

---
+++ 01.IntroductionToProgramming/03.ConsoleInputAndOutput/2.homework/01.NumbersInDiapason/NumbersInDiapason.cs
@@ -0,0 +1,23 @@
+﻿// 1. Write a program that reads two positive integer numbers and prints how many numbers p exist between them such that the reminder of the division with p by 5 is 0 (inclusive). Example: p(17,25) = 2
+using System;
+
+class NumbersInDiapason
+{
+    static void Main()
+    {
+        Console.Write("Enter the first number: ");
+        uint n1 = uint.Parse(Console.ReadLine());
+        Console.Write("Enter the second number: ");
+        uint n2 = uint.Parse(Console.ReadLine());
+        uint counter = 0;
+
+        for (uint i = n1; i <= n2; i++)
+        {
+            if (i % 5 == 0)
+            {
+                counter++;
+            }
+        }
+        Console.WriteLine("Numbers that the reminder of the division with 5 is 0 are: {0}", counter);
+    }
+}
---
+++ 01.IntroductionToProgramming/03.ConsoleInputAndOutput/2.homework/04.SumOfNNumbers/SumOfNNumbers.cs
@@ -0,0 +1,18 @@
+﻿//4. Write a program that gets a number n and after that gets more n numbers and calculates and prints their sum.
+using System;
+class SumOfNNumbers
+{
+    static void Main()
+    {
+        Console.Write("How many numbers will you enter: ");
+        uint count = uint.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for (int i = 0; i < count; i++)
+        {
+            Console.Write("Enter a number: ");
+            sum += int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Sum of the numbers is: {0}", sum);
+    }
+}
