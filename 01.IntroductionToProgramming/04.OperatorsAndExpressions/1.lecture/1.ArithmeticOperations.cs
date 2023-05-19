8abc0e529631ae064b777b645fe9eb748147f6bc
pixaily <danield.georgiev@gmail.com>
2023-05-19 14:38:07 +0300

    04. Operators and Expressions - Add lecture

---
+++ 01.IntroductionToProgramming/04.OperatorsAndExpressions/1.lecture/1.ArithmeticOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class ArithmenticOperations
+{
+    static void Main(string[] args)
+    {
+        int firstNumber = 7;
+        int secondNumber = 11;
+
+        Console.WriteLine("\n\nNumber one is: {0}",firstNumber);
+        Console.WriteLine("Number two is: {0}",secondNumber);
+        Console.WriteLine("The sum is: {0}",firstNumber+secondNumber);
+        Console.WriteLine("The product is: {0}",firstNumber*secondNumber);
+        Console.WriteLine("Division with remainder: {0} rem {1}",secondNumber/firstNumber,secondNumber%firstNumber);
+
+        Console.WriteLine(firstNumber+(secondNumber++));
+        Console.WriteLine("\n\nNumber one is: {0}", firstNumber);
+        Console.WriteLine("Number two is: {0}", secondNumber);
+        Console.WriteLine(firstNumber+++secondNumber);
+        Console.WriteLine("\n\nNumber one is: {0}", firstNumber);
+        Console.WriteLine("Number two is: {0}", secondNumber);
+        int number = 5 + 5 * 23;
+
+        Console.WriteLine("\n\n\n\n");
+        Console.WriteLine(13.0/7);
+        Console.WriteLine(0/5);
+        Console.WriteLine(5.0/0);
+        Console.WriteLine(-5.0/0);
+        Console.WriteLine(0.0/0);
+
+        Console.WriteLine((double)firstNumber/secondNumber);
+    }
+}
+
---
+++ 01.IntroductionToProgramming/04.OperatorsAndExpressions/1.lecture/5.OtherOperators.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class OtherOperators
+{
+    static void Main(string[] args)
+    {
+        string word1 = "Zariba";
+        string word2 = "Academy";
+        string result = word1 + word2;
+        Console.WriteLine(result);
+
+        int number = 17;
+        bool result1 = ((number % 2) == 1) ? true : false;
+        Console.WriteLine(result1);
+
+        Console.WriteLine(typeof(List<int>));
+        Console.WriteLine(sizeof(int));
+
+        //Type Conversions
+        //explicit
+        double someNumber = 5.05;
+        float anotherNumber = (float)someNumber;
+
+        //implicit
+        int evenAnotherNumber = 32;
+        long evenAnotherNumberNumber = evenAnotherNumber;
+    }
+}
+
