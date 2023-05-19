1bd70ccb7335d729abcc4fd713c26b9647ef2913
pixaily <danield.georgiev@gmail.com>
2023-05-19 14:58:06 +0300

    10. Strings - Add lecture

---
+++ 01.IntroductionToProgramming/10.Strings/1.lecture/3.UsefulStringManipulations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class OtherMethods
+{
+    static void Main()
+    {
+        //Replacing
+        string bunnyNames = "Pesho+Joro Senior+Gosho Junior";
+        string properBunnyNames = bunnyNames.Replace("+", " and ");
+        Console.WriteLine(bunnyNames);
+        Console.WriteLine(properBunnyNames);
+        Console.WriteLine();
+        //Removing strings
+        string price = "$123456";
+        string cutPrice = price.Remove(2,3);
+        Console.WriteLine(cutPrice);
+        Console.WriteLine();
+        //To Upper To lower
+        string bunnyUpper = bunnyNames.ToUpper();
+        Console.WriteLine(bunnyUpper);
+        string bunnyLower = bunnyNames.ToLower();
+        Console.WriteLine(bunnyLower);
+
+        //Trim, TrimStart, Trim end
+        string logo = "      \t\n\n\n Zariba    \n\n\n\n ";
+        Console.WriteLine(logo.Trim()+"1");
+        Console.WriteLine(logo.TrimStart()+"1");
+        Console.WriteLine(logo.TrimEnd()+"1");
+    }
+}
+
---
+++ 01.IntroductionToProgramming/10.Strings/1.lecture/4.0.StringsCanBeSlow.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class PlusSignForStringsIsBad
+{
+    static void Main(string[] args)
+    {
+        DateTime start = DateTime.Now;
+        string testString = DuplicateChars('a',10000);
+        DateTime end = DateTime.Now;
+        Console.WriteLine(end-start);
+        DateTime start1 = DateTime.Now;
+        string testString1 = DuplicateChars('a', 100000);
+        DateTime end1 = DateTime.Now;
+        Console.WriteLine(end1 - start1);
+    }
+    static string DuplicateChars(char symbol, int numberOfChars)
+    {
+        string result = "";
+        for (int i = 0; i < numberOfChars; i++)
+        {
+            result = result + symbol;
+        }
+        return result;
+    }
+}
+
